39e5fa03df28814ce4b350ebb9aff141a14e1a71
MrBhite <8209210208@csu.edu.cn>
2023-03-19 11:47:50 +0800

    indexDone

--- src/main/java/com/zlp/jpetstore_spring/controller/PetController.java
+++ src/main/java/com/zlp/jpetstore_spring/controller/PetController.java
@@ -1,12 +1,15 @@
 package com.zlp.jpetstore_spring.controller;
 
 import com.zlp.jpetstore_spring.service.PetService;
+import com.zlp.jpetstore_spring.service.impl.PetServiceImpl;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 /**
  * @author Raymond Li
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/Pet")
 public class PetController {
+
     @Autowired
     private PetService petService;
 
@@ -26,17 +30,20 @@ public class PetController {
 
     @GetMapping("/Index.html")
     public String showIndex(Model model, HttpSession httpSession){
-        String[] names = {"狗狗","猫咪","小宠","水族","爬虫"};
-        String[] listNames = {"dogList","catList","smallList", "fishList", "pythonList"};
-        /*for(String name:names){
-            model.addAttribute(name+"List",petService.getPetProductListByCategory("name"));
-        }*/
-        model.addAttribute("list",petService.getPetProductListByCategory("狗狗"));
+        setModel(model);
         return "/Pet/Index";
     }
 
     @GetMapping("/Search.html")
     public String showSearch(){
         return "/Pet/Search";
+    }
+
+    public void setModel(Model model){
+        String[] names = {"狗狗","猫咪","小宠","水族","爬虫"};
+        String[] listNames = {"dogList","catList","smallList", "fishList", "pythonList"};
+        for(int i = 0; i<5; i++){
+            model.addAttribute(listNames[i],petService.getPetProductListByCategory(names[i]));
+        }
     }
 }
