bcbe9e28709a8358ce82ff2324d4714c2af508c7
Raymond <raymond631@qq.com>
2023-06-17 09:07:18 +0800

    修复图片路径bug

--- src/main/java/com/example/jpetstore/Common/FilePathMapConfig.java
+++ src/main/java/com/example/jpetstore/Common/FilePathMapConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class FilePathMapConfig implements WebMvcConfigurer {
     private static final String staticResourcePath = "/image/look/**";
-    private static final String mapToLocalPath = "file:~/PetImage/";
+    private static final String mapToLocalPath = "file:/home/raymond/PetImage/";
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
